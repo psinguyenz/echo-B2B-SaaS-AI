65e5e10da4efe7d3d384e1035941a19a34ef4193
Hoahong2002 <psinguyenz@gmail.com>
2026-01-01 12:08:13 +0700

    02: convex package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button";
 // treat the 'convex' folder in backend like a 'src' folder
 
 export default function Page() {
-  const users = useQuery(api.users.getMany)
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <p>apps/web</p>
+      <Button onClick={() => addUser()}>Add</Button>
       <div className="max-w-sm w-full mx-auto">
         {JSON.stringify(users, null, 2)}
       </div>
