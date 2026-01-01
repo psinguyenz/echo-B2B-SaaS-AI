70afc30f298c2bbb79a6459fc0e97771fd05c339
Hoahong2002 <psinguyenz@gmail.com>
2026-01-01 11:19:28 +0700

    added backend convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,12 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/convex/_generated/api"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany)
+
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2,2)}</p>
-      </div>
+      <p>apps/web</p>
     </div>
   )
 }
